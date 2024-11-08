79cb8765f9d4a09add9617eb5177dc8252003771
OSHAN AVISHKA <153918928+oshanavishkapiries@users.noreply.github.com>
2024-11-08 23:32:49 +0530

    bug-fixed

--- src/main/java/com/invento/invento/controller/InventoryController.java
+++ src/main/java/com/invento/invento/controller/InventoryController.java
@@ -1,10 +1,12 @@
 package com.invento.invento.controller;
 
+import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
@@ -28,11 +30,19 @@ public class InventoryController {
             com_borderpane.setCenter(gridView);
 
             gridButton.setOnAction(event -> {
+                FadeTransition fadeTransition = new FadeTransition(Duration.millis(300), gridView);
+                fadeTransition.setFromValue(0.0);
+                fadeTransition.setToValue(1.0);
                 com_borderpane.setCenter(gridView);
+                fadeTransition.play();
             });
 
             listButton.setOnAction(event -> {
+                FadeTransition fadeTransition = new FadeTransition(Duration.millis(300), listViewContent);
+                fadeTransition.setFromValue(0.0);
+                fadeTransition.setToValue(1.0);
                 com_borderpane.setCenter(listViewContent);
+                fadeTransition.play();
             });
 
         } catch (IOException e) {
